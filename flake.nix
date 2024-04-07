470a51338981536502979867120f836f209c10c3
pandalanax <oskar.g@posteo.net>
2024-04-07 13:07:25 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "A simple NixOS flake";
+
+  inputs = {
+    # NixOS official package source, using the nixos-23.11 branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    conduit = {
+      url = "gitlab:famedly/conduit";
+
+      # Assuming you have an input for nixpkgs called `nixpkgs`. If you experience
+      # build failures while using this, try commenting/deleting this line. This
+      # will probably also require you to always build from source.
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs-stable.follows = "nixpkgs";
+    };
+    # Matrix
+    nix-matrix-appservices.url = "gitlab:coffeetables/nix-matrix-appservices";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    sops-nix,
+    nix-matrix-appservices,
+    nixpkgs-unstable,
+    ...
+  } @ inputs: {
+    nixosConfigurations.sorry2004 = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = {
+        inherit inputs;
+      };
+      modules = [
+        # Import the previous configuration.nix we used,
+        # so the old configuration file still takes effect
+        ./configuration.nix
+        ./matrix.nix
+        # sops-nix.nixosModules.sops
+        nix-matrix-appservices.nixosModule
+        {
+          nixpkgs.overlays = [
+            (final: prev: {
+              mautrix-signal = nixpkgs-unstable.legacyPackages.x86_64-linux.mautrix-signal;
+            })
+          ];
+        }
+      ];
+    };
+  };
+}
